2516cd481417e6089bd83f9de0877ab94e83dd06
JohanK <klausenjohan23@gmail.com>
2021-09-27 12:40:33 +0200

    server is alive

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'using strict'
 
 const http = require('http');
-
-const 
+var fs = require('fs');
+http.createServer((req, res)=> {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('im alive!');
+    res.end();
+  }).listen(8080);
